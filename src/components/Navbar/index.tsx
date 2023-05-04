6ed93ab5693786b57035d5f8200343bcb481d2d6
pujiragil <ragilsg164@gmail.com>
2023-05-04 22:34:35 +0700

    added nav size

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,5 +1,23 @@
 const Navbar = () => {
-  return <div className="font-semibold">Navbar</div>;
+  return (
+    <nav className="flex justify-between items-center">
+      <img
+        className="h-10 w-auto object-cover"
+        src="/cofile.png"
+        alt="cofile"
+      />
+      <img
+        className="w-8 h-8 object-cover md:hidden"
+        src="/hamburger.svg"
+        alt="hamburger"
+      />
+      <ul className="hidden md:flex items-center gap-10 text-base font-medium text-black/70">
+        <li className="navlink">Home</li>
+        <li className="navlink">About us</li>
+        <li className="navlink">Pricing</li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
