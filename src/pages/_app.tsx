627cab457a04af1a59249ae4a23609df6915baef
pujiragil <ragilsg164@gmail.com>
2023-05-04 21:58:43 +0700

    applied epilogues font google

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Epilogue } from "next/font/google";
+
+const epilogue = Epilogue({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={epilogue.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
