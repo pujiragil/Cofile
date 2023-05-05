2da9efc5130ae3c94a810a83cd40181803ccb3de
pujiragil <ragilsg164@gmail.com>
2023-05-05 10:19:28 +0700

    created Hero component

---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,14 @@
+const Hero = () => {
+  return (
+    <section className="py-4 px-6">
+      <div className="pt-12 space-y-2">
+        <h1 className="text-4xl font-semibold text-primary-100 leading-[48px]">
+          Check Currency Exchange Rate in Real Time
+        </h1>
+        <p className="text-primary-200 leading-6">Monitor your worldwide business with this handy apps</p>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
