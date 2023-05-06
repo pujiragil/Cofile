59651aa9f8ad898f141086f0ed10630cc3abc11c
pujiragil <ragilsg164@gmail.com>
2023-05-06 07:26:09 +0700

    created CardTestimonials component

---
+++ src/components/Card/CardTestimonial.tsx
@@ -0,0 +1,56 @@
+import { FC } from "react";
+
+interface CardItemProps {
+  description: string;
+  name: string;
+  job: string;
+  avatar: string;
+}
+
+const CardItem: FC<CardItemProps> = ({ avatar, name, job, description }) => {
+  return (
+    <div className="space-y-12">
+      <img className="w-24 h-24 object-cover" src={avatar} alt="testimonial" />
+      <div className="space-y-2">
+        <p className="font-semibold text-4xl text-secondary-100">“ ”</p>
+        <div className="flex flex-col leading-6">
+          <h3 className="text-primary-100 font-semibold pb-2 border-b border-primary-300">
+            {description}
+          </h3>
+          <div className="space-y-4 pt-2">
+            <p className="text-primary-100">{name}</p>
+            <p className="text-primary-200">{job}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const CardTestimonials = () => {
+  return (
+    <section className="px-6 flex justify-center">
+      <div className="max-w-[908px] space-y-1">
+        <h2 className="text-2xl font-semibold text-primary-100 leading-32">
+          Testimonials
+        </h2>
+        <div className="py-8 flex flex-col gap-12">
+          <CardItem
+            avatar="/testimonial-1.png"
+            name="Ashley Rock"
+            job="Founder of Paple"
+            description="Really happy with the product and kind services provided by Cofile. Will recommend it to the world!"
+          />
+          <CardItem
+            avatar="/testimonial-2.png"
+            name="Natalie Bloom"
+            job="Marketing of Simpage"
+            description="Top class product and services! Since my first sign up, they helped my team a lot."
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default CardTestimonials;
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,5 +1,6 @@
 export { default as Navbar } from "./Navbar";
 export { default as Hero } from "./Hero";
 export { default as SponsorShip } from "./SponsorShip";
-export { default as CardMain } from "./Card"
-export { default as CardFeatures } from "./Card/CardFeature"
+export { default as CardMain } from "./Card";
+export { default as CardFeatures } from "./Card/CardFeature";
+export { default as CardTestimonials } from "./Card/CardTestimonial";
