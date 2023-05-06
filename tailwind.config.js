4b48da1907f31800b1d8a8f812e3b1d036194e4e
pujiragil <ragilsg164@gmail.com>
2023-05-06 07:26:44 +0700

    new color pallete

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,10 @@ module.exports = {
         primary: {
           100: "#202020",
           200: "#A3A3A3",
+          300: "#DEDEDE",
+        },
+        secondary: {
+          100: "#0570EB",
         },
       },
     },
