da67b18305d5967880b7c0032023af468800d7a9
pujiragil <ragilsg164@gmail.com>
2023-05-07 22:34:29 +0700

    created AboutBanner component

---
+++ src/components/About/AboutBanner.tsx
@@ -0,0 +1,11 @@
+const AboutBanner = () => {
+  return (
+    <img
+      className="h-[260px] w-full object-cover sm:h-[320px] md:h-[400px]"
+      src="/about.png"
+      alt="about"
+    />
+  );
+};
+
+export default AboutBanner;
