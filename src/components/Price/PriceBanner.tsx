5bbdd9c9de499f1f8046e57625f4db284626162b
pujiragil <ragilsg164@gmail.com>
2023-05-08 21:51:33 +0700

    created priceBanner component

---
+++ src/components/Price/PriceBanner.tsx
@@ -0,0 +1,11 @@
+const PriceBanner = () => {
+  return (
+    <img
+      className="h-[260px] w-full object-cover sm:h-[320px] md:h-[400px]"
+      src="/pricing-plan.png"
+      alt="pricing-plan"
+    />
+  );
+};
+
+export default PriceBanner;
