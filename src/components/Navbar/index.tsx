aaa2a2beb23137dbf69f558ba9dc6a1dbe65f6d5
pujiragil <ragilsg164@gmail.com>
2023-05-09 10:04:29 +0700

    created dropdown navigation

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import datas from "../../data/content.json";
 import { useRouter } from "next/router";
+import { useState } from "react";
 
 interface NavbarContentProps {
   iconImg: string;
@@ -9,28 +10,64 @@ interface NavbarContentProps {
 
 const Navbar = () => {
   const router = useRouter();
+  const [isOpen, setIsOpen] = useState<boolean>(false);
   const { iconImg, links } = datas.navbar as NavbarContentProps;
 
   return (
-    <nav className="container mx-auto flex justify-between items-center py-2 px-6">
-      <img className="h-10 w-auto object-cover" src={iconImg} alt="cofile" />
-      <img
-        className="w-8 h-8 object-cover cursor-pointer md:hidden"
-        src="/hamburger.svg"
-        alt="hamburger"
-      />
-      <ul className="hidden md:flex items-center gap-10 text-base font-medium text-black/70">
-        {links.map((link) => (
-          <li
-            key={link.id}
-            className={`navlink ${
-              router.pathname === link.path ? "text-black" : ""
-            }`}
-          >
-            <Link href={link.path}>{link.name}</Link>
-          </li>
-        ))}
-      </ul>
+    <nav className="relative">
+      <div className="container mx-auto flex justify-between items-center py-2 px-6 pt-6">
+        <Link href="/">
+          <img
+            className="h-10 w-auto object-cover"
+            src={iconImg}
+            alt="cofile"
+          />
+        </Link>
+        <img
+          onClick={() => setIsOpen(true)}
+          className="w-8 h-8 object-cover cursor-pointer md:hidden"
+          src="/hamburger.svg"
+          alt="hamburger"
+        />
+        <ul className="hidden md:flex items-center gap-10 text-base font-medium text-black/70">
+          {links.map((link) => (
+            <li
+              key={link.id}
+              className={`navlink ${
+                router.pathname === link.path ? "text-black" : ""
+              }`}
+            >
+              <Link href={link.path}>{link.name}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div
+        className={`absolute top-0 left-0 bg-white w-full px-6 py-6 flex justify-between items-start drop-shadow-lg md:hidden ${
+          isOpen ? "translate-y-0" : "-translate-y-full"
+        } transition-all duration-500 ease-in-out`}
+      >
+        <ul className="grid gap-5 h-fit text-black/70 text-base font-semibold">
+          {links.map((link) => (
+            <li
+              key={link.id}
+              onClick={() => setIsOpen(false)}
+              className={`navlink ${
+                router.pathname === link.path ? "text-black" : ""
+              }`}
+            >
+              <Link href={link.path}>{link.name}</Link>
+            </li>
+          ))}
+        </ul>
+        <img
+          onClick={() => setIsOpen(false)}
+          className="w-8 h-8 object-cover cursor-pointer"
+          src="/hamburger-close.svg"
+          alt="hamburger-close"
+        />
+      </div>
     </nav>
   );
 };
