8ae6a67668addad17a6e8c42c118973d5637dd4c
pujiragil <ragilsg164@gmail.com>
2023-05-09 20:12:27 +0700

    replace .png to .webp

--- src/components/About/AboutBanner.tsx
+++ src/components/About/AboutBanner.tsx
@@ -2,8 +2,8 @@ const AboutBanner = () => {
   return (
     <img
       className="h-[260px] w-full object-cover sm:h-[320px] md:h-[400px]"
-      src="/about.png"
-      alt="about"
+      src="/banner.webp"
+      alt="about-banner"
     />
   );
 };
--- src/components/Price/PriceBanner.tsx
+++ src/components/Price/PriceBanner.tsx
@@ -2,8 +2,8 @@ const PriceBanner = () => {
   return (
     <img
       className="h-[260px] w-full object-cover sm:h-[320px] md:h-[400px]"
-      src="/pricing-plan.png"
-      alt="pricing-plan"
+      src="/banner.webp"
+      alt="pricing-banner"
     />
   );
 };
